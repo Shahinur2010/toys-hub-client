aedf346acd21088f3aa5ba3b31eb792e61032a16
Shahinur2010 <shahinurmony4@gmail.com>
2023-05-19 12:15:45 +0600

    firebase config file hidden using .env.local file creation

--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -1,10 +1,65 @@
 
+import React, { useEffect, useState } from 'react';
+import { createContext } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
-const AuthProvider = () => {
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleAuthProvider)
+    }
+
+    const updateUser = () => {
+        return updateProfile(auth.currentUser, {
+            displayName: "Programming Hero", photoURL: "https://example.com/jane-q-user/profile.jpg"
+        })
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    // observe auth state change
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        updateUser,
+        logOut
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
